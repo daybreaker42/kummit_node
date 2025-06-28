66112666b6228f311eec1514522fcb7ee380d886
한성준 <june123000@naver.com>
2025-06-28 23:41:22 +0900

    3003포트로 수정

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express'); // Express 프레임워크 import
 const app = express(); // Express 애플리케이션 인스턴스 생성
-const PORT = 3000; // 서버 포트 설정
+const PORT = 3003; // 서버 포트 설정
 
 // /hello 엔드포인트 정의 - GET 요청 시 "Hello World" 반환
 app.get('/hello', (req, res) => {
