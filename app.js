888babb3749a8556052e052b8bd779ae1d9962cc
한성준 <june123000@naver.com>
2025-06-29 00:13:28 +0900

    add: 도전 기록 목록 조회 추가

--- app.js
+++ app.js
@@ -7,7 +7,22 @@ app.get('/hello', (req, res) => {
     res.send('Hello World'); // 클라이언트에게 "Hello World" 문자열 응답
 });
 
+// 도전 기록 목록 조회
+app.get('/api/users/me', (req, res) => {
+  // json data 반환
+  const data = {
+    "userId": 0,
+    "username": "kuit",
+    "tier": "SILVER",
+    "level": 2,
+    "language": "Java",
+    "rating": 2,
+    "streak_days": 2
+  };
+  res.json(data);
+});
+
 // 서버 시작 및 포트 리스닝
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => { // '0.0.0.0'으로 바인딩하여 모든 인터페이스에서 접근 가능하도록 설정
     console.log(`Server is running on http://localhost:${PORT}`); // 서버 시작 확인 메시지
 });
